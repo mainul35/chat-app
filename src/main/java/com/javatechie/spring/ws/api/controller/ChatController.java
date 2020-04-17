9f4d86a0da9e13dafb252887312b5727a446bac7
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-04-18 00:22:14 +0600

    initial commit

---
+++ src/main/java/com/javatechie/spring/ws/api/controller/ChatController.java
@@ -0,0 +1,57 @@
+package com.javatechie.spring.ws.api.controller;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.javatechie.spring.ws.api.model.ChatMessage;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@Controller
+public class ChatController {
+
+	final SimpMessagingTemplate simpMessagingTemplate;
+
+	public ChatController(SimpMessagingTemplate messagingTemplate) {
+		this.simpMessagingTemplate = messagingTemplate;
+	}
+
+	@GetMapping("/")
+	public String root(Model model) {
+		model.addAttribute("roomId", UUID.randomUUID().toString());
+		return "index";
+	}
+
+/*	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}*/
+
+	@MessageMapping("/chat.register/{roomId}")
+	public void register(@DestinationVariable String roomId, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		simpMessagingTemplate.convertAndSend("/topic/"+roomId, chatMessage);
+	}
+
+	/*@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}*/
+
+	@MessageMapping("/chat.send/{roomId}")
+	public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
+		simpMessagingTemplate.convertAndSend("/topic/"+roomId, chatMessage);
+	}
+
+}
